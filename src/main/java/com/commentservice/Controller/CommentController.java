7d23e2d3db69658ac0c8b80592eec1af804abdcd
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-21 22:47:05 +0530

    Feature added - get comments by post id

--- src/main/java/com/commentservice/Controller/CommentController.java
+++ src/main/java/com/commentservice/Controller/CommentController.java
@@ -1,13 +1,25 @@
 package com.commentservice.Controller;
 
 
+import com.commentservice.Model.CommentModel;
 import com.commentservice.Service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CommentController {
 
     @Autowired
     private CommentService commentService;
+
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity<List<CommentModel>> showCommentsByPostId(@PathVariable("postId") String postId){
+        return new ResponseEntity<>(commentService.showCommentsByPostId(postId), HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/commentservice/Service/CommentService.java
+++ src/main/java/com/commentservice/Service/CommentService.java
@@ -1,13 +1,21 @@
 package com.commentservice.Service;
 
 
+import com.commentservice.Model.CommentModel;
 import com.commentservice.Repository.CommentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommentService {
 
     @Autowired
     private CommentRepo commentRepo;
+
+
+    public List<CommentModel> showCommentsByPostId(String postId){
+        return commentRepo.findBypostID(postId);
+    }
 }
