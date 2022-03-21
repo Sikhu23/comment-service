de93f37588e62d894503afab184d25a60976047c
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-21 23:26:32 +0530

    feature added - update comment based on commentId

--- src/main/java/com/commentservice/Service/CommentService.java
+++ src/main/java/com/commentservice/Service/CommentService.java
@@ -1,13 +1,24 @@
 package com.commentservice.Service;
 
 
+import com.commentservice.Model.CommentModel;
 import com.commentservice.Repository.CommentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CommentService {
 
     @Autowired
     private CommentRepo commentRepo;
+
+    public CommentModel updateComment(CommentModel commentModel, String postId,String commentId){
+        commentModel.setCommentID(commentId);
+        commentModel.setUpdatedAt(LocalDateTime.now());
+        commentModel.setCreatedAt(commentRepo.findById(commentId).get().getCreatedAt());
+        commentModel.setPostID(postId);
+        return commentRepo.save(commentModel);
+    }
 }
