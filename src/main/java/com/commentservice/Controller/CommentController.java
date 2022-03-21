546d2fe244a2955879e7a85f8fcf778f8b2ddbc4
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-21 23:11:06 +0530

    Feature added - get comment by commentID

--- src/main/java/com/commentservice/Controller/CommentController.java
+++ src/main/java/com/commentservice/Controller/CommentController.java
@@ -1,8 +1,13 @@
 package com.commentservice.Controller;
 
 
+import com.commentservice.Model.CommentModel;
 import com.commentservice.Service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +15,10 @@ public class CommentController {
 
     @Autowired
     private CommentService commentService;
+
+
+    @GetMapping("/posts/{postId}/comments/{commentId}")
+    public ResponseEntity<CommentModel> findByCommentId(@PathVariable("postId") String postId,@PathVariable("commentId") String commentId){
+        return new ResponseEntity<>(commentService.findByCommentId(commentId), HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/commentservice/Service/CommentService.java
+++ src/main/java/com/commentservice/Service/CommentService.java
@@ -1,6 +1,7 @@
 package com.commentservice.Service;
 
 
+import com.commentservice.Model.CommentModel;
 import com.commentservice.Repository.CommentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,8 @@ public class CommentService {
 
     @Autowired
     private CommentRepo commentRepo;
+
+    public CommentModel findByCommentId(String commentId){
+        return this.commentRepo.findById(commentId).get();
+    }
 }
